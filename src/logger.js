bf649b207fef71a3c46d3085c72ae70cfcf8ef1a
PerthuisQuentin <perthuis.quentin@gmail.com>
2016-12-15 00:31:59 +0100

    [Logger] Create logger

---
+++ src/logger.js
@@ -0,0 +1,23 @@
+const Winston = require('winston');
+
+var logger = new Winston.Logger({
+    level: 'info',
+    transports: [
+	    new (Winston.transports.Console)({
+	    	name: 'debug-console',
+	    	level: 'debug',
+	    	colorize: true,
+	    	timestamp: true,
+	    	showLevel: true
+	    }),
+	    new (Winston.transports.File)({
+      		name: 'error-file',
+      		filename: 'filelog-error.log',
+      		level: 'error',
+      		timestamp: true,
+      		showLevel: true
+    	})
+	]
+});
+
+module.exports = logger;
