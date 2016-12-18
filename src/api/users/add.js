d8f67867c86af1e35e32f257c3cf8cb3a096d627
PerthuisQuentin <perthuis.quentin@gmail.com>
2016-12-18 02:41:25 +0100

    [API][Users/Add] Start Joi validation

--- src/api/users/add.js
+++ src/api/users/add.js
@@ -3,7 +3,26 @@ const Config = require('../../config.js');
 
 module.exports.options = {
 	method: 'POST',
-	path: '/api/users/add'
+	path: '/api/users/add',
+	level: Config.rolesLevel.guest,
+	validation : {
+		options: {
+
+		},
+		schema : {
+			email: Joi.string()
+				.email()
+				.description("Email of the user")
+				.required(),
+			nickname: Joi.string()
+				.min(3).max(30)
+				.description("Nickame that will be displayed")
+				.required(),
+			password: Joi.string()
+				.description("Password of the user")
+				.required()
+		}
+	}
 };
 
 module.exports.handler = function(request, response) {
