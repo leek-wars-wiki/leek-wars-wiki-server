49d2fbf7574dbf28afce407850755969b599e643
PerthuisQuentin <perthuis.quentin@gmail.com>
2017-01-31 23:48:07 +0100

    [Routes] Add routes for scripts and styles

--- src/setup.js
+++ src/setup.js
@@ -21,18 +21,6 @@ module.exports.register = function (server, options, next) {
         isCached: false
     });
 
-    server.route({
-	    path: "/ressources/{path*}",
-	    method: "GET",
-	    handler: {
-	        directory: {
-	            path: "./src/views",
-	            listing: false,
-	            index: false
-	        }
-	    }
-	});
-
     loadRouteFolder(server, {
 		path: API_PATH,
 		requirePath: API_REQUIRE_PATH,
